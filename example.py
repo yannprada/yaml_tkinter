cbdc27b161ed50a2b5a3f160de003364a3531e47
yannprada <pradayrol.yannick@gmail.com>
2025-06-27 19:20:24 +0200

    Builder is now an object, and it also needs an object with methods for command bindings already defined

--- example.py
+++ example.py
@@ -1,29 +1,19 @@
 import tkinter as tk
-from yamltk import build
+from yamltk import Builder
+
+
+# this class is used to bind methods
+# and maybe other things in the future? FIXME
+class Application:
+    # define this method, so that it is bound automatically
+    # since it is referenced in the yaml file
+    def on_button_exit(self):
+        quit()
+    
+    # TODO: example with manual event binding (command)
 
 
 if __name__ == '__main__':
-    root, widgets = build('./example.yaml')
-    
-    button = widgets['exit_button'] # retrieve the button by id
-    button = root.nametowidget('!frame.!button') # or follow the hierarchy
-    
-    button.configure(command=quit)
-    
-    entry_var = tk.StringVar()
-    widgets['entry'].configure(textvariable=entry_var)
-    widgets['entry_label'].configure(textvariable=entry_var)
-    
-    radio_var = tk.StringVar()
-    widgets['foo_radio'].configure(variable=radio_var)
-    widgets['bar_radio'].configure(variable=radio_var)
-    widgets['radio_label'].configure(textvariable=radio_var)
-    
-    # TODO: make a new version of yamltk.py that uses a Builder object
-    # benefits:
-    #   keep some references without having to pass them on the recursion chain
-    # references to keep:
-    #   marked widgets with 'id'
-    #   tk variables (StringVar, ...)
-    
-    root.mainloop()
+    app = Application()
+    builder = Builder('./example.yaml', app)
+    builder.root.mainloop()
--- yamltk.py
+++ yamltk.py
@@ -6,50 +6,63 @@ except ImportError:
     from yaml import Loader, Dumper
 
 
-def build(filename):
-    with open(filename) as f:
-        data = yaml.load(f, Loader)
-        return _build_widget(data)
-
-
-def _build_widget(data, marked_widgets={}, parent=None):
-    widget_name = next(iter(data.keys()))
-    if not hasattr(tk, widget_name):
-        raise TypeError(f'Invalid widget: {widget_name}')
+class Builder:
+    tk_widgets = {}
+    tk_variables = {}
     
-    widget_class = getattr(tk, widget_name)
-    widget = widget_class(parent)
-    options = widget.configure()
-    data = data[widget_name]
+    def __init__(self, filename, application):
+        self.application = application
+        with open(filename) as f:
+            data = yaml.load(f, Loader)
+            self.root = self._build_widget(data)
     
-    for key, value in data.items():
-        match key:
-            case 'widget':
-                continue
-            case 'children':
-                for child_data in value:
-                    child_widget, marked_widgets = _build_widget(child_data, marked_widgets, widget)
-                continue
-            case 'id':
-                marked_widgets[value] = widget
-            case _:
-                if hasattr(widget, key):
-                    method = getattr(widget, key)
-                    method(value)
-                elif key in options:
-                    widget.configure(**{key: value})
-                else:
-                    raise AttributeError(f'Invalid attribute "{key}" for widget "{widget_name}"')
+    def _build_widget(self, data, parent=None):
+        widget_name = next(iter(data.keys()))
+        if not hasattr(tk, widget_name):
+            raise TypeError(f'Invalid widget: {widget_name}')
+        
+        widget_class = getattr(tk, widget_name)
+        widget = widget_class(parent)
+        options = widget.configure()
+        data = data[widget_name]
+        
+        for key, value in data.items():
+            match key:
+                case 'widget':
+                    continue
+                case 'children':
+                    for child_data in value:
+                        child_widget = self._build_widget(child_data, widget)
+                    continue
+                case 'id':
+                    self.tk_widgets[value] = widget
+                case 'var':
+                    widget.configure(variable=self._get_string_var(value))
+                case 'int_var':
+                    widget.configure(textvariable=self._get_int_var(value))
+                case 'text_var':
+                    widget.configure(textvariable=self._get_string_var(value))
+                case 'command':
+                    cmd = getattr(self.application, value)
+                    widget.configure(command=cmd)
+                case _:
+                    if hasattr(widget, key):
+                        method = getattr(widget, key)
+                        method(value)
+                    elif key in options:
+                        widget.configure(**{key: value})
+                    else:
+                        raise AttributeError(f'Invalid attribute "{key}" for widget "{widget_name}"')
+        
+        return widget
     
-    return widget, marked_widgets
-
-
-if __name__ == '__main__':
-    root, marked_widgets = build('./example.yaml')
+    def _get_var(self, name, var_class):
+        var = self.tk_variables.get(name, var_class())
+        self.tk_variables[name] = var
+        return var
     
-    button = marked_widgets['click_me_button']
-    # or
-    button = root.nametowidget('!frame2.!button')
+    def _get_int_var(self, name):
+        return self._get_var(name, tk.IntVar)
     
-    button.configure(command=quit)
-    root.mainloop()
+    def _get_string_var(self, name):
+        return self._get_var(name, tk.StringVar)

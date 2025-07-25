74d248f4c6fffe2ec22c280900d81bc1aefad9e8
yannprada <pradayrol.yannick@gmail.com>
2025-07-25 16:33:08 +0200

    Example: modified comments, converted Branch.yaml_file to absolute paths.

--- src/example/example.py
+++ src/example/example.py
@@ -3,43 +3,60 @@ sys.path.append('..')
 
 # copy everything below for the README
 
+"""
+You can find explanations in the comments here, or inside the different yaml 
+files of this example project.
+
+I suggest starting by reading the `if __name__ == '__main__':` section at the 
+bottom of this file.
+"""
+
+from pathlib import Path
 from tk_double_scale import DoubleScale
 import tkinter as tk
 import yamltk
 
 
-# The name must match in the yaml file
 class Root(tk.Tk):
-    # Specify the YAML file that defines the layout.
-    # path must be absolute, or relative to the directory running the script
-    yaml_file = 'root.yaml'
+    """This is the root branch.
+
+    The name can be anything, but it must match the one inside the yaml file.
     
-    # Executed after the widget has been built
+    yaml_file is the path to the layout file (yaml).
+    It can be a relative path if you are sure from where the script will be 
+    executed.
+    """
+    yaml_file = Path(__file__).parent / 'root.yaml'
+    
     def post_build(self):
+        """This method is always executed after the widget has been built."""
         self.button_add.configure(command=self.on_add_button)
 
     def on_add_button(self):
-        # Use the builder to add a new 'Item' branch inside the bottom_frame
-        # The name parameter is in the tkinter sense
-        # Here self.bottom_frame is valid because of the name parameter 
-        # in the yaml file
+        """
+        The method below adds a new branch ('Item') to the specified 
+        parent frame (bottom_frame) in the user interface. 
+        The branch is created without a specific name for the item.
+        """
         self.builder.add_branch(branch_name='Item', name=None, 
                                 parent=self.bottom_frame)
 
-    # Define this method, so that it is bound automatically
-    # since it is referenced in the example.yaml file
     def on_checkbutton_foobar(self):
-        # here is how to access tk variables
+        """
+        This method must be defined, since it is referenced in the yaml file.
+        You can see below how variables defined in the yaml file can be accessed.
+        """
         foo = 'foo' if self.check_foo_var.get() else ''
         bar = 'bar' if self.check_bar_var.get() else ''
         self.check_foobar_var.set(f'{foo} {bar}')
 
 
-# Branch example
 class TitleFrame(tk.Frame):
-    yaml_file = 'title_frame.yaml'
+    """Another branch example."""
+    yaml_file = Path(__file__).parent / 'title_frame.yaml'
     
     def post_build(self, title, color, foo=None, quz=0):
+        """Here we can find the arguments passed in the root.yaml file."""
         self.title_label.configure(
             text=title, fg=color
         )
@@ -50,12 +67,14 @@ class TitleFrame(tk.Frame):
 
 
 class Item(tk.Frame):
-    yaml_file = 'item.yaml'
+    yaml_file = Path(__file__).parent / 'item.yaml'
 
 
 if __name__ == '__main__':
-    # A Root branch is required to build the application (name can be anything)
-    # other branches are optional
+    """
+    A Root branch is required to build the application (name can be anything).
+    Other branches are optional.
+    """
     root = yamltk.build(Root, [TitleFrame, Item, DoubleScale])
     root.radio_var.set('tea')
     root.mainloop()
